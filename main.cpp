1809e0377725fe99944bc4239399a58735c15bcb
constantinos030 <constantinos.joh@student.mahidol.edu>
2026-01-28 04:55:47 +0000

    Finished

--- main.cpp
+++ main.cpp
@@ -6,26 +6,39 @@
 //
 
 #include <iostream>
+#include <iomanip>
+
 using namespace std;
+#include "sorting.h"
 
 int main(int argc, char * argv[]) {
     
  
   cout<<"argc is "<<argc<<endl;
-    int i,N;
-    int *a;
-    for(i=1;i<argc;i++){
+    int N=argc-1;
+    int *a = new int[N];
+    for(int i=1;i<argc;i++){
         cout<<"argv[" << i<<"]" <<argv[i]<<endl;
       
     }
-    /* Convert to numbers*/
-    N=argc-1;
-    cout<<"Before sorting:";
-    display(a,N);
     
-    sort(a,N);
-    cout<<"After sorting:";
+    /* Convert to numbers*/
+   
+
+    for (int i = 1; i<argc; i++)
+    {
+      a[i-1] = atoi(argv[i]);
+    }
+
+     cout<<"Before sorting: ";
     display(a,N);
+    sort(a, N);
+    
+    
+    cout<<"After sorting: ";
+    display(a, N);
+    
+    delete a;
     
     
     return 0;
--- sorting.h
+++ sorting.h
@@ -1,3 +1,43 @@
 void swap(int &a,int &b);
 void sort(int *a,int N);
 void display(int *a,int N);
+
+void display(int *a, int N)
+{
+    for (int i = 0; i<N ; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+void sort(int *a, int N)
+{
+bool sort_check = false;
+
+
+do{ 
+for (int i = 0; i<N-1; i++){if (a[i]>a[i+1]) swap(a[i], a[i+1]);}
+
+{ // Sorting Checker
+int check_count = 0;
+for(int i = 0; i<N-1; i++)
+{
+    if (a[i]<a[i+1] || a[i]==a[i+1] ) check_count++;
+}
+if (check_count == N-1) sort_check = true;
+}
+
+}while (sort_check == false);
+
+
+}
+
+void swap(int &a, int &b)
+{
+    int temp;
+
+    temp=b;
+    b=a;
+    a=temp;
+}
